a82a315b7b132f717d1ebb52ce16e18439062f37
Signe M-S <117270262+signeskuldbol@users.noreply.github.com>
2025-12-01 17:16:39 +0100

    error 12.26 RMSE: 13.63 STD 5.82 from R chances

--- CNN_based_sulution/LightGlue_all_drones.py
+++ CNN_based_sulution/LightGlue_all_drones.py
@@ -1454,9 +1454,9 @@ for i, img_path in enumerate(sorted(DRONE_IMG_CLEAN.iterdir())):
             pos_base_std=55.0, # in px. measured the difference of GT and meas for first run and found mean around 55 px, with 95 percentile around 10 and 123 px
             heading_base_std_rad=np.deg2rad(3.0), # a normal good measurement are seen to be within +-3 degrees
             overall_conf=best_conf_overall,  # between 0 and 1
-            pos_min_scale=0.5, # controls how much we trust low confidence measurements sets the unceartanty
-            pos_max_scale=2.0,  # controls how much we trust high confidence measurements sets the certainty
-            heading_min_scale=0.5, # same for heading -||- determined using 95 percentile
+            pos_min_scale=0.5, # controls how much we trust low confidence measurements sets the unceartanty determined using 95 percentile
+            pos_max_scale=2.0,  # controls how much we trust high confidence measurements sets the certainty -||-
+            heading_min_scale=0.5, # same for heading -||- found through testing
             heading_max_scale=2.75  # same for heading -||-
         )  # this gives us R matrix for EKF update. R tells us how certain we are about the measurements.
 
